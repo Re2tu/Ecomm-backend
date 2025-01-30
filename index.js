b8822d275cbf03c3bb9d1da32ca099817dfd691c
Dumpa Reethika <dumpareethika@gmail.com>
2025-01-30 20:11:23 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 // app.use(cors({
 //   origin: ['https://ecomm-frontend-beryl.vercel.app', 'https://ecomm-admin-lime.vercel.app'], // Allow frontend and admin URLs
@@ -23,27 +23,29 @@ app.use(express.json());
 
 
 // List of allowed origins (both admin and frontend URLs)
-const allowedOrigins = [
-  'http://localhost:5173',  // Admin page during development
-  'http://localhost:3000',  // Frontend page during development
-  'https://ecomm-admin-lime.vercel.app',  // Deployed admin URL
-  'https://ecomm-frontend-beryl.vercel.app',  // Deployed frontend URL
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',  // Admin page during development
+//   'http://localhost:3000',  // Frontend page during development
+//   'https://ecomm-admin-lime.vercel.app',  // Deployed admin URL
+//   'https://ecomm-frontend-beryl.vercel.app',  // Deployed frontend URL
+// ];
 
 // CORS middleware setup to allow multiple origins
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      // Allow the request if the origin is in the allowed list, or no origin (for Postman/CLI requests)
-      callback(null, true);
-    } else {
-      // Reject requests from unknown origins
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST','PUT', 'DELETE'],  // Allow specific HTTP methods if needed
-  credentials: true,  // Allow cookies if needed
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       // Allow the request if the origin is in the allowed list, or no origin (for Postman/CLI requests)
+//       callback(null, true);
+//     } else {
+//       // Reject requests from unknown origins
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST','PUT', 'DELETE'],  // Allow specific HTTP methods if needed
+//   credentials: true,  // Allow cookies if needed
+// }));
+
+
 
 const baseUrl = process.env.BASE_URL;
 console.log("baseUrl:", baseUrl);
